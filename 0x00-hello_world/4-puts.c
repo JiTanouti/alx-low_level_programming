001da82d970c38a197b48029824abd5127f165b5
JiTanouti <tanoutijihad@gmail.com>
2023-08-10 15:11:40 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(void){
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+
+/**
+ * main - Entry point
+ *
+ * Description: a C program that prints using the function puts
+ * 
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+         puts("\"Programming is like building a multilingual puzzle");
+         return(0);
 }	
